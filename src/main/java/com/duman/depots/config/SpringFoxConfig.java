7c154d55c35d2390aa20b3ac4e3cb49b8a186d74
ahmtdumn <ahmttdmn@gmail.com>
2022-05-29 13:54:58 +0300

    Swagger eklendi

--- src/main/java/com/duman/depots/config/SpringFoxConfig.java
+++ src/main/java/com/duman/depots/config/SpringFoxConfig.java
@@ -2,19 +2,36 @@ package com.duman.depots.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 @Configuration
+@EnableSwagger2
 public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.duman.depots"))
+                .paths(PathSelectors.regex("/.*"))
+                .build().apiInfo(apiEndPointsInfo());
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Stok İşlemleri")
+                .description("Depots Api")
+                .contact(new Contact("ahmet", "", "ahmttdmn@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
                 .build();
     }
 }
